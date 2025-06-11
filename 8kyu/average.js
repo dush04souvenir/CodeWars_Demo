46ae4cf1b30fbb44e7a6aa19eec5242b7c74cd43
souvenir DUSHIME <dusouvenir04@gmail.com>
2025-06-11 21:10:44 +0200

    weeek2-wednesday

---
+++ 8kyu/average.js
@@ -0,0 +1,18 @@
+// Write a function which calculates the average of the numbers in a given array.
+
+// Note: Empty arrays should return 0.
+function findAverage(array) {
+  // your code 
+  if(array.length === 0){
+    return 0;
+  }
+  else{
+    return array.reduce((a,b) => a + b, 0) / array.length;
+  }
+  
+  
+}
+// best practice solution
+function findAverage(array) {
+  return array.length ? array.reduce((a, b) => a + b) / array.length : 0;
+}
